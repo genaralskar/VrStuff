a9c587c6d93aaa423b5e988e88abd2cc12c4979a
Unknown <genaralskar1@gmail.com>
2018-04-28 20:15:22 -0600

    Vr Movement! Holy Butts! update git.ignore

---
+++ VR Stuff/Assets/MyStuff/PrimativesCreator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimativesCreator : MonoBehaviour {
+
+	private SteamVR_TrackedController _controller;
+	private PrimitiveType _currentPrimitiveType = PrimitiveType.Sphere;
+
+	void OnEnable()
+	{
+		_controller = GetComponent<SteamVR_TrackedController>();
+		_controller.TriggerClicked += HandleTriggerClicked;
+		_controller.PadClicked += HandlePadClicked;
+	}
+
+	void OnDisable()
+	{
+		_controller.TriggerClicked -= HandleTriggerClicked;
+		_controller.PadClicked -= HandlePadClicked;
+	}
+
+	#region Primative Spawning
+	void HandleTriggerClicked(object sender, ClickedEventArgs e)
+	{
+		SpawnCurrentPrimativeAtController();
+	}
+
+	void SpawnCurrentPrimativeAtController()
+	{
+		var spawnedPrimative = GameObject.CreatePrimitive(_currentPrimitiveType);
+		spawnedPrimative.transform.position = transform.position;
+		spawnedPrimative.transform.rotation = transform.rotation;
+
+		spawnedPrimative.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+		if(_currentPrimitiveType == PrimitiveType.Plane)
+		{
+			spawnedPrimative.transform.localScale = new Vector3(.01f, .01f, .01f);
+		}
+	}
+	#endregion
+
+	#region  PrimativeSelection
+
+	void HandlePadClicked(object sender, ClickedEventArgs e)
+	{
+		if(e.padY < 0)
+			SelectPreviousPrimitive();
+		else
+			SelectNextPrimitive();
+	}
+
+	void SelectNextPrimitive()
+	{
+		_currentPrimitiveType++;
+		if(_currentPrimitiveType > PrimitiveType.Quad)
+			_currentPrimitiveType = PrimitiveType.Sphere;
+	}
+
+	void SelectPreviousPrimitive()
+	{
+		_currentPrimitiveType--;
+		if(_currentPrimitiveType < PrimitiveType.Sphere)
+		{
+			_currentPrimitiveType = PrimitiveType.Quad;
+		}
+	}
+	#endregion
+
+}
---
+++ VR Stuff/Assets/MyStuff/VrPlayerMove.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrPlayerMove : MonoBehaviour {
+
+	public SteamVR_TrackedController _controller;
+	public GameObject _camera;
+	public CharacterController _player;
+	public bool padInput = false;
+
+	public float moveSpeed = 1;
+
+	void OnEnable()
+	{
+		_controller.PadTouched += HandlePadTouched;
+		_controller.PadUntouched += HandlePadUntouched;
+	}
+
+	void OnDisable()
+	{
+		_controller.PadTouched -= HandlePadTouched;
+		_controller.PadUntouched -= HandlePadUntouched;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void HandlePadTouched(object sender, ClickedEventArgs e)
+	{
+
+		Vector3 targetDirection = new Vector3(e.padX, 0f, e.padY);
+		targetDirection = _camera.transform.TransformDirection(targetDirection);
+		targetDirection.y = 0.0f;
+
+		//StartCoroutine(Move(targetDirection));
+		StartCoroutine(Move(e));
+	}
+
+	IEnumerator Move(ClickedEventArgs e)
+	{
+		while(true)
+		{
+			yield return new WaitForEndOfFrame();
+
+			Vector3 targetDirection = new Vector3(Input.GetAxis("Primary Pad X"), 0f, Input.GetAxis("Primary Pad Y"));
+			
+			targetDirection = _camera.transform.TransformDirection(targetDirection);
+			targetDirection.y = 0.0f;
+
+			targetDirection = targetDirection.normalized;
+			print(targetDirection);
+
+			_player.Move(targetDirection * moveSpeed * Time.deltaTime);
+		}
+		
+	}
+
+	void HandlePadUntouched(object sender, ClickedEventArgs e)
+	{
+		StopAllCoroutines();
+	}
+}
